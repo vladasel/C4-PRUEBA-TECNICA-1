bedda09421a180e878ad6c59718f009020cc9939
vladasel <ionelavladasel2710@gmail.com>
2022-08-23 13:26:56 +0200

    update controller grupo

--- src/main/java/com/ionela/rest/controller/GrupoController.java
+++ src/main/java/com/ionela/rest/controller/GrupoController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ionela.rest.dto.Grupo;
+
 import com.ionela.rest.service.GrupoServiceImpl;
 
 @RestController
@@ -19,5 +22,11 @@ public class GrupoController {
 	@GetMapping("/grupos")
 	public List<Grupo> listarGrupos() {
 		return grupoServiceImpl.listarGrupos();
+	}
+	
+	@PostMapping("/grupos")
+	public Grupo salvarGrupo(@RequestBody Grupo grupo) {
+
+		return grupoServiceImpl.guardarGrupo(grupo);
 	}
 }
