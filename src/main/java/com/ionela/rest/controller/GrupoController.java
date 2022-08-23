539005caea5b609b2c1f77343a756ebfdd402be7
vladasel <ionelavladasel2710@gmail.com>
2022-08-23 13:24:53 +0200

    start doing GrupoCOntroller

--- src/main/java/com/ionela/rest/controller/GrupoController.java
+++ src/main/java/com/ionela/rest/controller/GrupoController.java
@@ -1,5 +1,23 @@
 package com.ionela.rest.controller;
 
-public class GrupoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ionela.rest.dto.Grupo;
+import com.ionela.rest.service.GrupoServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class GrupoController {
+	@Autowired
+	GrupoServiceImpl grupoServiceImpl;
+
+	@GetMapping("/grupos")
+	public List<Grupo> listarGrupos() {
+		return grupoServiceImpl.listarGrupos();
+	}
 }
